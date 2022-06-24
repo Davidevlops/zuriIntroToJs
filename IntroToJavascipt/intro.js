d5fe5ca4a4774643b658b12e6dd150a940bb3562
David <91037972+Davidevlops@users.noreply.github.com>
2022-06-24 11:01:20 +0100

    Add files via upload

---
+++ IntroToJavascipt/intro.js
@@ -0,0 +1,5 @@
+const fullname = "David Eniola Olabode";
+const height = "6 meters";
+const country = "Nigeria";
+const myDescription = `my names are ${fullname}, I am ${height} tall and I reside in ${country} `;
+document.write(myDescription);
